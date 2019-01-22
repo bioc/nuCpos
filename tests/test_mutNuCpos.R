4c731b937db39e6b565d37810c2eaa1b86d50ea8
hkatomed <hkato@med.shimane-u.ac.jp>
2019-01-22 17:07:57 +0900

    Parameters (linker length distribution for budding and fission yeasts) in sysdata.rda were replaced. Version bumped to 1.1.3.

--- tests/test_mutNuCpos.R
+++ tests/test_mutNuCpos.R
@@ -10,8 +10,8 @@ test_mutNuCpos <- function(){
         ylim.HBA = c(-11, 0), 
         annotation = data.frame(name = "alpha2", 
         color = "purple", left = 1534, right = 1559), full = TRUE)
-    expect_equal(results$results$pstart[101], 0.0005225962, tolerance = 1.0e-8)
-    expect_equal(results$results$nucoccup[101], 0.9885672, tolerance = 1.0e-6)
+    expect_equal(results$results$pstart[101], 0.0005697254, tolerance = 1.0e-8)
+    expect_equal(results$results$nucoccup[101], 0.9874718, tolerance = 1.0e-6)
     expect_equal(results$results$viterbi[101], 1)
     expect_equal(results$results$affinity[101], -0.89849, tolerance = 1.0e-5)
     expect_equal(results$results$pos[101], -3673)
